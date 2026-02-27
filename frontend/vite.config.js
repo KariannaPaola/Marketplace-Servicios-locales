1272c825d35db23f2a103c8a1e316aa918d55a37
KariannaPaola <kariannaarriaga07@gmail.com>
2026-02-27 12:44:50 -0800

    acccct

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,5 +3,8 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: './', // importante para que build funcione en Vercel
+  base: '/', // importante para que build funcione en Vercel
+  build: {
+    outDir: "dist",   // La carpeta que Vercel sirve
+  },
 });
