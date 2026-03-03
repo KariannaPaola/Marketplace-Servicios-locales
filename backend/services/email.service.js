2d84481e76da94e00c5828f1a26da8019bd643e4
KariannaPaola <kariannaarriaga07@gmail.com>
2026-03-03 14:05:09 -0800

    update

--- backend/services/email.service.js
+++ backend/services/email.service.js
@@ -14,15 +14,28 @@
  * @module SendEmail
  */
 
-import transporterEmail from '../config/mailer.js';
+import sgMail from '@sendgrid/mail';
 
-const sendEmail = async ({ to, subject, html }) => {
-  await transporterEmail.sendMail({
-    from: `"ServiYa" <${process.env.MAIL_USER}>`,
-    to,
-    subject,
-    html
-  });
+// Configura tu clave de API de SendGrid
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+
+// Función para enviar correos
+const sendEmail = async (to, subject, html) => {
+  const msg = {
+    to, // Dirección de destino
+    from: process.env.MAIL_USER, // Correo verificado en SendGrid
+    subject, // Asunto del correo
+    html, // Cuerpo del correo en HTML
+  };
+
+  try {
+    // Enviar el correo usando la API de SendGrid
+    await sgMail.send(msg);
+    console.log('Correo enviado exitosamente');
+  } catch (error) {
+    console.error('Error al enviar el correo:', error);
+  }
 };
 
 export default sendEmail;
+
